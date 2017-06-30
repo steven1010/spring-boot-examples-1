0e400159af10a21be176829fea1b705ff4e3d149
ropert911 <ropert911@163.com>
2017-06-30 23:52:24 +0800

    添加js report的测试工程，待调通

---
+++ spring-boot-study/st_jsreport/src/main/java/com/xq/st_jsreport/App.java
@@ -0,0 +1,13 @@
+package com.xq.st_jsreport;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
